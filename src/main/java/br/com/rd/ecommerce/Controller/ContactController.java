50951fcc758025046cc1953732fe6956127d0d50
Treinamento <Treinamento@RD723925.raiadrogasil.com>
2020-03-18 15:39:38 -0300

    + contato ok

---
+++ src/main/java/br/com/rd/ecommerce/Controller/ContactController.java
@@ -0,0 +1,29 @@
+package br.com.rd.ecommerce.Controller;
+
+import br.com.rd.ecommerce.model.Client;
+import br.com.rd.ecommerce.model.Contact;
+import br.com.rd.ecommerce.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+    @Autowired
+    private ContactRepository repository;
+    @ResponseStatus(HttpStatus.CREATED)
+
+//    @PostMapping("/create-contact")
+//    public Contact save(@RequestBody Contact contact){
+//        return repository.save(contact);   }
+
+    @PostMapping("/create-contact")
+    public Contact save(@RequestBody Contact contact){
+        return repository.save(contact);
+    };
+}
+
--- src/main/java/br/com/rd/ecommerce/model/Contact.java
+++ src/main/java/br/com/rd/ecommerce/model/Contact.java
@@ -2,10 +2,12 @@ package br.com.rd.ecommerce.model;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
